9e7b69d4ba4653de8567937acb415a305ad5c1c0
Bobby Ciullo <bobby.ciullo@ucf.edu>
2019-07-19 09:30:18 -0400

    removed header and footer

--- blank-template.php
+++ blank-template.php
@@ -5,10 +5,10 @@ Description: This template has no included elements and is meant to be used for 
 */
 ?>
 
-<?php  get_header(); ?>
+<?php // get_header(); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 <?php the_content(__('(more...)')); ?>
 <?php endwhile; else: ?>
         <p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
-<?php  get_footer(); ?>
+<?php // get_footer(); ?>
